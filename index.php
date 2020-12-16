7cb92c62ed89b81fdc09a5677035208cac51e457
Daniela <danielamirandaa9@gmail.com>
2020-12-15 22:48:55 -0300

    Adicionando validação de campos no formulário

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html>
+
+<head>
     <meta charset="utp-8">
     <title>Formulário de Inscrição</title>
     <meta name="author" content="">
     <meta name="description" content="">
     <meta name="viewport" content="width-device-width, initial-scale-1">
+</head>
 
 <body>
     <p>Formulário para Inscrição de Competidores</p>
 
     <form action="script.php" method="post">
-        <p>Seu nome: <input type="text" name="nome"></p>
-        <p>Sua idade: <input type="text" name="idade"></p>
-        <p><input type="submit" value="Enviar"></p>
+        <p>
+            Seu nome: <input type="text" name="nome">
+        </p>
+        <p>
+            Sua idade: <input type="text" name="idade">
+        </p>
+        <p>
+            <input type="submit" value="Enviar">
+        </p>
     </form>
 </body>
+
 </html>
